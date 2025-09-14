ca004fb4ad085b2f7bbeb686b134ad62f1b7e786
Aman Desai <amandesai01@gmail.com>
2025-09-14 23:27:24 +0530

    ui: Spinner

---
+++ app/components/Ui/spinner/Spinner.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { LoaderCircle } from 'lucide-vue-next';
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+
+interface SpinnerProps {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<SpinnerProps>();
+</script>
+
+<template>
+  <LoaderCircle :class="cn('animate-spin', props.class)" />
+</template>
