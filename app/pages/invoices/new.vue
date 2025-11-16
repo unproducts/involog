7ad2fa2ca214771ff5412e22757ebe899b70ffe9
Aman Desai <amandesai01@gmail.com>
2025-11-16 16:48:35 +0530

    chore: mount new pages

---
+++ app/pages/invoices/new.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ReceiptText } from 'lucide-vue-next';
+</script>
+
+<template>
+  <AppPage title="New Invoice" :icon="ReceiptText">
+    <InvoiceEditOrCreate />
+  </AppPage>
+</template>
