bf759232b8d94ebe055ab560951ba0fdbcf8be35
Aman Desai <amandesai01@gmail.com>
2025-11-17 14:23:34 +0530

    chore: invoice edit page

---
+++ app/pages/invoices/edit.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ReceiptText } from 'lucide-vue-next';
+
+const route = useRoute();
+
+const id = route.query.id as string;
+
+if (!id) {
+  await navigateTo('/invoices/new');
+}
+
+const { data: invoice } = useQuery(getInvoiceByIdColada({ id }));
+</script>
+
+<template>
+  <AppPage :title="`Edit Invoice ${invoice.number}`" :icon="ReceiptText" v-if="invoice">
+    <InvoiceEditOrCreate :invoice />
+  </AppPage>
+</template>
