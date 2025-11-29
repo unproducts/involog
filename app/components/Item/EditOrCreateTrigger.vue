3f636b14662b9328b9b54d2d97723dbb57d4ac74
sumitbadase <56994822+sumitbadase@users.noreply.github.com>
2025-11-29 15:32:33 +0530

    Fixes [29/11] (#4)
    * chore: fix units selection
    
    * chore: update items service

--- app/components/Item/EditOrCreateTrigger.vue
+++ app/components/Item/EditOrCreateTrigger.vue
@@ -1,44 +1,39 @@
 <script setup lang="ts">
 import type { ItemSchema } from '~~/shared/schemas/item';
-import type EditOrCreate from './EditOrCreate.vue';
 
 const props = defineProps<{
   item?: ItemSchema;
 }>();
 
-const editOrCreateRef = ref<InstanceType<typeof EditOrCreate>>();
-
-const isUpdating = !!props.item;
-
-const open = ref(false);
-const status = ref<DataStateStatus>('pending');
+const modalOpen = ref(false);
 const loading = ref(false);
 
-watch(status, (newStatus) => {
-  if (newStatus === 'success') {
-    open.value = false;
-  }
-});
+const editOrCreateRef = useTemplateRef('editOrCreateRef');
 </script>
 
 <template>
-  <ShadDialog v-model:open="open">
+  <ShadDialog v-model:open="modalOpen">
     <ShadDialogTrigger as-child>
       <slot />
     </ShadDialogTrigger>
     <ShadDialogContent class="sm:max-w-[425px]">
       <ShadDialogHeader>
-        <ShadDialogTitle>{{ isUpdating ? 'Edit' : 'New' }} {{ item?.name || 'Item' }}</ShadDialogTitle>
+        <ShadDialogTitle>{{ item ? 'Edit' : 'New' }} {{ item?.name || 'Item' }}</ShadDialogTitle>
         <ShadDialogDescription sr-only>
-          {{ isUpdating ? 'Make changes to your item here.' : 'Add a new item to your system.' }}
+          {{ item ? 'Make changes to your item here.' : 'Add a new item to your system.' }}
           {{ item?.name || 'Item' }}
           Click save when you're done.
         </ShadDialogDescription>
       </ShadDialogHeader>
-      <ItemEditOrCreate ref="editOrCreateRef" :item="item" v-model:status="status" v-model:loading="loading" />
+      <ItemEditOrCreate
+        ref="editOrCreateRef"
+        :item="item"
+        @update:loading="loading = $event"
+        @submitted="modalOpen = false"
+      />
       <ShadDialogFooter>
         <ShadButton type="submit" @click="editOrCreateRef?.submit()" :disabled="loading">
-          {{ isUpdating ? 'Update' : 'Create' }}
+          {{ item ? 'Update' : 'Create' }}
         </ShadButton>
       </ShadDialogFooter>
     </ShadDialogContent>
